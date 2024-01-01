30be67e929bf4472a984f2bf3e686a137fa81825
Aman Pandia <pandiaaman16@gmail.com>
2024-01-01 23:56:34 +0530

    updates

--- src/main/java/com/pandiaaman/springboot2024refresher/Springboot2024refresherApplication.java
+++ src/main/java/com/pandiaaman/springboot2024refresher/Springboot2024refresherApplication.java
@@ -8,6 +8,9 @@ public class Springboot2024refresherApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot2024refresherApplication.class, args);
+		System.out.println("*** running app ***");
 	}
+	
+	
 
 }
