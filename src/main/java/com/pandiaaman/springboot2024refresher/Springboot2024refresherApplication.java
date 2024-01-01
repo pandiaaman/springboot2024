8645f0974a7cad1db1e7a7a34f1b3402aaa9530d
Aman Pandia <pandiaaman16@gmail.com>
2024-01-01 21:41:08 +0530

    first commit

---
+++ src/main/java/com/pandiaaman/springboot2024refresher/Springboot2024refresherApplication.java
@@ -0,0 +1,13 @@
+package com.pandiaaman.springboot2024refresher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2024refresherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2024refresherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pandiaaman/springboot2024refresher/Springboot2024refresherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandiaaman.springboot2024refresher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2024refresherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
