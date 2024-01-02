de2a8841ccfe1dd554592742dd4b5ca692ddf9d9
Aman Pandia <pandiaaman16@gmail.com>
2024-01-02 10:54:20 +0530

    actuator updates

---
+++ src/test/java/com/pandiaaman/springboot2024refresher/junit/MyMathTest.java
@@ -0,0 +1,14 @@
+package com.pandiaaman.springboot2024refresher.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyMathTest {
+
+	@Test
+	void testCalculateSum() {
+		fail("Not yet implemented");
+	}
+
+}
