b43fbae4903ef0f150bc438d0a050bd257f2cf45
Aman Pandia <pandiaaman16@gmail.com>
2024-01-05 14:35:04 +0530

    jpa hibernate completion

--- src/main/java/com/pandiaaman/springboot2024refresher/course/entity/Course.java
+++ src/main/java/com/pandiaaman/springboot2024refresher/course/entity/Course.java
@@ -1,5 +1,10 @@
 package com.pandiaaman.springboot2024refresher.course.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,12 +16,15 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Entity
 public class Course {
 
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name="ID")
 	private int courseId;
-	
+	@Column(name="NAME")
 	private String courseName;
-	
+	@Column(name="AUTHOR")
 	private String courseAuthor;
 }
