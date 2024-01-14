691ce227c879fd87882761ea5ba5758c051027f8
Aman Pandia <pandiaaman16@gmail.com>
2024-01-14 23:53:28 +0530

    validation updates

--- src/main/java/com/pandiaaman/springboot2024refresher/socialmedia/user/errors/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/pandiaaman/springboot2024refresher/socialmedia/user/errors/CustomizedResponseEntityExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -40,8 +41,14 @@ public abstract class CustomizedResponseEntityExceptionHandler extends ResponseE
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(
 			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 
-		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),ex.getMessage(),request.getDescription(false));
+		String allValidationErrors = "";
 		
+		for(FieldError err: ex.getFieldErrors()) {
+			allValidationErrors += err;
+		}
+		
+//		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),ex.getFieldError().getDefaultMessage(),request.getDescription(false));
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),allValidationErrors,request.getDescription(false));
 		
 		return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
 	}
